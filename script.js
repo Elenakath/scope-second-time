449a2ac309b22467d452a63828f2f3a04c705497
elena_e <elenaka123@gmail.com>
2024-04-10 11:13:40 -0500

    Inital commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const numOfDrinks = 5;
+const drinks = function () {
+    const tea = 6 + numOfDrinks;
+    console.log(tea);
+};
+
+drinks();
+
+if (numOfDrinks === 5) {
+    let soda = "lemon-lime";
+    console.log(soda);
+}
